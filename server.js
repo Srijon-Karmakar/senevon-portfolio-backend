9699d4378e1fc4b4592fca44e6da2a84bd9c6bb5
Srijon-Karmakar <srijonkarmakar333@gmail.com>
2025-09-15 14:46:17 +0530

    emberassed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-app.options("*", cors());
+app.options(cors());  
 
 // Security + parsers
 app.use(helmet({ crossOriginResourcePolicy: false }));
